b7dca027a0715f401a35f590dc70fdbc045c1381
Uriell77 <hermoso77@gmail.com>
2025-05-26 13:50:39 -0400

    herocomponent

---
+++ src/lib/components/HeroBanner.svelte
@@ -0,0 +1,59 @@
+<script>
+  import HeroButtons from '$lib/components/HeroButtons.svelte';
+
+  let {usuario} = $props();
+</script>
+
+
+<section class="hero pb-4 is-justify-content-center" style="background-image:url('{usuario.store.storebanner}')">
+
+  <!-- hero buttoms -->
+  <HeroButtons />
+
+  <!-- hero body -->
+  <div class="hero-body py-5">
+    <div class="logo">
+      <img src="{usuario.store.storelogo}" alt="logo" loading="lazy">
+    </div>
+    <h1 class="has-text-weight-bold">{usuario.store.storename}</h1>
+    <h4>{usuario.store.storequote}</h4>
+  </div>
+
+  <!-- hero tabs -->
+  <div class="contenttab is-flex">
+  <!--  <Tabs /> -->
+  </div>
+
+</section>
+
+<style>
+  .logo{
+    position:absolute;
+    top:50%;
+    left:50%;
+    transform:translate(-50%,-120%);
+    width:100px;
+    height:100px;
+    border-radius:30px;
+  }
+
+  .hero-body{
+    background-color: rgba(200,200,200,.4);
+    backdrop-filter: blur(4px);
+    border-radius:30px;
+    border-style:solid;
+    border-width:thin;
+    border-color: rgba(1,1,1,.3);
+  }
+
+  .hero{
+    background-color: rgba(200,200,255,.3) !important;
+    overflow: hidden;
+    background-size:cover;
+    background-position:center center;
+    background-repeat:no-repeat;
+    background-attachment: center;
+    align-items:center;
+  }
+
+</style>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,14 +1,15 @@
 <script>
+  import Hero from '$lib/components/HeroBanner.svelte';
   import NavTab from '$lib/components/NavTab.svelte';
   import Tabs from '$lib/components/Tabs.svelte';
-  import HeroButtons from '$lib/components/HeroButtons.svelte';
   import Content from '$lib/components/Content.svelte';
 
 
   let usuario1 = {
     "store": {"storename": "Nombre Tienda",
       "storequote": "Su compra es nuestra Prioridad",
-      "storelogo": "./check.png"
+      "storelogo": "./check.png",
+      "storebanner": "/clinic.jpeg"
     }
   }
 
@@ -20,27 +21,8 @@
 
 </script>
 
+<Hero usuario={usuario1}/>
 
-<section class="hero pb-4 is-justify-content-center">
-
-  <!-- hero buttoms -->
-  <HeroButtons />
-
-  <!-- hero body -->
-  <div class="hero-body is-pulled-right py-5">
-    <div class="logo">
-      <img src="{usuario1.store.storelogo}" alt="logo" loading="lazy">
-    </div>
-    <h1 class="has-text-weight-bold">{usuario1.store.storename}</h1>
-    <h4>{usuario1.store.storequote}</h4>
-  </div>
-
-  <!-- hero tabs -->
-  <div class="contenttab is-flex">
-  <!--  <Tabs /> -->
-  </div>
-
-</section>
 
 <!-- Content -->
 
@@ -63,40 +45,6 @@
 
 
 <style>
-
-
-  .logo{
-    position:absolute;
-    top:50%;
-    left:50%;
-    transform:translate(-50%,-120%);
-    width:100px;
-    height:100px;
-    border-radius:30px;
-  }
-
-  .hero-body{
-    background-color: rgba(200,200,200,.4);
-    backdrop-filter: blur(4px);
-    border-radius:30px;
-    border-style:solid;
-    border-width:thin;
-    border-color: rgba(1,1,1,.3);
-  }
-
-  .hero{
-    background-color: rgba(200,200,255,.3) !important;
-    overflow: hidden;
-    background-image: url('/clinic.jpeg');
-    background-size:cover;
-    background-position:center center;
-    background-repeat:no-repeat;
-    background-attachment: center;
-    align-items:center;
-  }
-
-
-
 
   .section{
     height:100%;
